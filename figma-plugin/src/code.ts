9a3c24703e3d1dac7ac9de5f6f3217c6c3763e22
caelum <caelum119@gmail.com>
2020-06-14 19:49:32 +0100

    Adjust plugin size

--- figma-plugin/src/code.ts
+++ figma-plugin/src/code.ts
@@ -6,7 +6,7 @@
 // full browser enviroment (see documentation).
 
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__, {width: 232, height: 208 });
+figma.showUI(__html__, {width: 10 * 30, height: 16 * 30 });
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
